dd7bef4195b688635a09e4efb989338978ea6104
zhaorui <czhaor@gmail.com>
2023-09-04 17:38:29 +0800

    update chpt3.

--- toy_example/build.sh
+++ toy_example/build.sh
@@ -1,3 +1,4 @@
-cmake -S ./ -B `pwd`/build -G Ninja -DLLVM_DIR=/usr/lib/llvm-16/lib/cmake/llvm/ \
-  -DMLIR_DIR=/usr/lib/llvm-16/lib/cmake/mlir/
+cmake -S ./ -B `pwd`/build -G Ninja
+# XXX: use following env compile if LLVM is manually installed
+# -DLLVM_DIR=/usr/lib/llvm-16/lib/cmake/llvm/ -DMLIR_DIR=/usr/lib/llvm-16/lib/cmake/mlir/
 cmake --build `pwd`/build --target all
--- toy_example/run.sh
+++ toy_example/run.sh
@@ -11,3 +11,13 @@
 # /usr/local/opt/llvm/bin/mlir-tblgen -gen-op-defs ./ch2/include/toy/Ops.td -I /usr/local/opt/llvm/include/
 ./build/ch2/toyc-ch2 ./ch2/codegen.toy -emit=mlir -mlir-print-debuginfo &> ch2.mlir
 ./build/ch2/toyc-ch2 ./ch2.mlir -emit=mlir &> ch2.roundtrip.mlir
+
+# chpt3
+# /usr/local/opt/llvm/bin/mlir-tblgen -gen-rewriters ./ch3/mlir/ToyCombine.td -I /usr/local/opt/llvm/include/
+# NOTE: implemented in ToyCombine.cpp
+./build/ch3/toyc-ch3 ./ch3/transpose.toy -emit=mlir -opt -mlir-print-debuginfo &> ch3_opt_trans.mlir
+./build/ch3/toyc-ch3 ./ch3/transpose.toy -emit=mlir -mlir-print-debuginfo &> ch3_wo_opt_trans.mlir
+
+# NOTE: implemented in ToyCombine.td
+./build/ch3/toyc-ch3 ./ch3/reshape.toy -emit=mlir -opt -mlir-print-debuginfo &> ch3_opt_reshape.mlir
+./build/ch3/toyc-ch3 ./ch3/reshape.toy -emit=mlir -mlir-print-debuginfo &> ch3_wo_opt_reshape.mlir
