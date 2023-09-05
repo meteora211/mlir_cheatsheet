8b12aa094486cb255ab39ad25ec77a556e5bbca7
zhaorui <czhaor@gmail.com>
2023-09-05 18:19:51 +0800

    fix errors.

--- toy_example/run.sh
+++ toy_example/run.sh
@@ -5,10 +5,10 @@
 
 # chpt2
 # use following command to see generated sources:
-# /usr/local/opt/llvm/bin/mlir-tblgen -gen-op-defs ./ch2/include/toy/Ops.td -I /usr/local/opt/llvm/include/
-# /usr/local/opt/llvm/bin/mlir-tblgen -gen-op-defs ./ch2/include/toy/Ops.td -I /usr/local/opt/llvm/include/
 # /usr/local/opt/llvm/bin/mlir-tblgen -gen-op-decls ./ch2/include/toy/Ops.td -I /usr/local/opt/llvm/include/
 # /usr/local/opt/llvm/bin/mlir-tblgen -gen-op-defs ./ch2/include/toy/Ops.td -I /usr/local/opt/llvm/include/
+# /usr/local/opt/llvm/bin/mlir-tblgen -gen-dialect-decls ./ch2/include/toy/Ops.td -I /usr/local/opt/llvm/include/
+# /usr/local/opt/llvm/bin/mlir-tblgen -gen-dialect-defs ./ch2/include/toy/Ops.td -I /usr/local/opt/llvm/include/
 ./build/ch2/toyc-ch2 ./ch2/codegen.toy -emit=mlir -mlir-print-debuginfo &> ch2.mlir
 ./build/ch2/toyc-ch2 ./ch2.mlir -emit=mlir &> ch2.roundtrip.mlir
 
