77ca7ef7c1e0aeda2d931dc6124bf6ac15ee7277
zhaorui <czhaor@gmail.com>
2023-09-05 14:42:13 +0800

    update build

--- compile/build.sh
+++ compile/build.sh
@@ -1,19 +1,25 @@
 # sudo apt-get install clang lld
-git clone https://github.com/llvm/llvm-project.git
-mkdir llvm-project/build
-cd llvm-project/build
-cmake -G Ninja ../llvm \
+REPO_DIR=./llvm-project
+BUILD_DIR=./llvm-project/build
+INSTALL_DIR=./llvm-project/install
+git clone -b release/16.x https://github.com/llvm/llvm-project.git
+mkdir -p $BUILD_DIR 
+cmake -G Ninja "-S $REPO_DIR/llvm" \
+   -B $BUILD_DIR \
    -DLLVM_ENABLE_PROJECTS=mlir \
    -DLLVM_BUILD_EXAMPLES=ON \
    -DLLVM_TARGETS_TO_BUILD="Native;NVPTX;AMDGPU" \
-   -DCMAKE_BUILD_TYPE=Release \
-   -DLLVM_ENABLE_ASSERTIONS=ON
+   -DCMAKE_BUILD_TYPE=Debug \
+   -DLLVM_ENABLE_ASSERTIONS=ON \
+   -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
+   -DMLIR_INCLUDE_INTEGRATION_TESTS=OFF \
+   -DLLVM_INCLUDE_TOOLS=ON
 # Using clang and lld speeds up the build, we recommend adding:
- -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DLLVM_ENABLE_LLD=ON
+# -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DLLVM_ENABLE_LLD=ON
 # CCache can drastically speed up further rebuilds, try adding:
 #  -DLLVM_CCACHE_BUILD=ON
 # Optionally, using ASAN/UBSAN can find bugs early in development, enable with:
 # -DLLVM_USE_SANITIZER="Address;Undefined" 
 # Optionally, enabling integration tests as well
 # -DMLIR_INCLUDE_INTEGRATION_TESTS=ON
-cmake --build . --target check-mlir
+cmake --build $BUILD_DIR --target install
--- toy_example/build.sh
+++ toy_example/build.sh
@@ -1,4 +1,5 @@
-cmake -S ./ -B `pwd`/build -G Ninja
-# XXX: use following env compile if LLVM is manually installed
-# -DLLVM_DIR=/usr/lib/llvm-16/lib/cmake/llvm/ -DMLIR_DIR=/usr/lib/llvm-16/lib/cmake/mlir/
+# XXX: use following -DLLVM_DIR and -DLLVM_DIR env to compile if LLVM is manually installed
+cmake -S ./ -B `pwd`/build -G Ninja -DCMAKE_BUILD_TYPE=Debug \
+-DLLVM_DIR=../compile/llvm-project/install/lib/cmake/llvm/ -DMLIR_DIR=../compile/llvm-project/build/lib/cmake/mlir/
+# -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DLLVM_ENABLE_LLD=ON
 cmake --build `pwd`/build --target all
