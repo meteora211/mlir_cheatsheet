e1dab876cda955b644a2abe13221e066376957c9
zhaorui <czhaor@gmail.com>
2023-09-15 22:13:27 +0800

    chp5: how to convert dialect.

--- toy_example_execise/run.sh
+++ toy_example_execise/run.sh
@@ -16,3 +16,6 @@ LLVM_REPO_DIR=$(pwd)/../compile/llvm-project
 ./build/toyc ./tests/inline.toy -emit=mlir -mlir-print-debuginfo &> ./tests/ch4_wo_opt.mlir
 ./build/toyc ./tests/inline.toy -emit=mlir -opt -mlir-print-debuginfo &> ./tests/ch4_inline_only.mlir
 ./build/toyc ./tests/inline.toy -emit=mlir -opt -infer-shape -mlir-print-debuginfo &> ./tests/ch4_inline_infer_shape.mlir
+
+./build/toyc ./tests/convert.toy -opt -emit=mlir -mlir-print-debuginfo &> ./tests/ch5_origin.mlir
+./build/toyc ./tests/convert.toy -opt -emit=mlir-affine -mlir-print-debuginfo &> ./tests/ch5_affine.mlir
