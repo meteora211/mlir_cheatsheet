aaf53ceb519a771bc47d97c715de22a3ded56afc
zhaorui chen <czhaor@gmail.com>
2023-09-23 21:53:48 +0800

    chp5: lower to llvm.

--- toy_example_execise/run.sh
+++ toy_example_execise/run.sh
@@ -4,18 +4,23 @@ LLVM_REPO_DIR=$(pwd)/../compile/llvm-project
 # $LLVM_REPO_DIR/install/bin/mlir-tblgen -gen-dialect-defs ./include/toy/ToyDialect.td -I $LLVM_REPO_DIR/install/include/
 # $LLVM_REPO_DIR/install/bin/mlir-tblgen -gen-op-decls ./include/toy/ToyOps.td -I $LLVM_REPO_DIR/install/include/ -I ./include/toy
 # $LLVM_REPO_DIR/install/bin/mlir-tblgen -gen-op-defs ./include/toy/ToyOps.td -I $LLVM_REPO_DIR/install/include/ -I ./include/toy
-./build/toyc ./tests/codegen.toy -emit=mlir -mlir-print-debuginfo &> ./tests/ch2.mlir
-./build/toyc ./tests/ch2.mlir -emit=mlir &> ./tests/ch2.roundtrip.mlir
+# ./build/toyc ./tests/codegen.toy -emit=mlir -mlir-print-debuginfo &> ./tests/ch2.mlir
+# ./build/toyc ./tests/ch2.mlir -emit=mlir &> ./tests/ch2.roundtrip.mlir
 
-./build/toyc ./tests/transpose.toy -emit=mlir -opt -mlir-print-debuginfo &> ./tests/ch3_opt_trans.mlir
-./build/toyc ./tests/transpose.toy -emit=mlir -mlir-print-debuginfo &> ./tests/ch3_wo_opt_trans.mlir
+# ./build/toyc ./tests/transpose.toy -emit=mlir -opt -mlir-print-debuginfo &> ./tests/ch3_opt_trans.mlir
+# ./build/toyc ./tests/transpose.toy -emit=mlir -mlir-print-debuginfo &> ./tests/ch3_wo_opt_trans.mlir
 
-./build/toyc ./tests/reshape.toy -emit=mlir -opt -mlir-print-debuginfo &> ./tests/ch3_opt_reshape.mlir
-./build/toyc ./tests/reshape.toy -emit=mlir -mlir-print-debuginfo &> ./tests/ch3_wo_opt_reshape.mlir
+# ./build/toyc ./tests/reshape.toy -emit=mlir -opt -mlir-print-debuginfo &> ./tests/ch3_opt_reshape.mlir
+# ./build/toyc ./tests/reshape.toy -emit=mlir -mlir-print-debuginfo &> ./tests/ch3_wo_opt_reshape.mlir
 
-./build/toyc ./tests/inline.toy -emit=mlir -mlir-print-debuginfo &> ./tests/ch4_wo_opt.mlir
-./build/toyc ./tests/inline.toy -emit=mlir -opt -mlir-print-debuginfo &> ./tests/ch4_inline_only.mlir
-./build/toyc ./tests/inline.toy -emit=mlir -opt -infer-shape -mlir-print-debuginfo &> ./tests/ch4_inline_infer_shape.mlir
+# ./build/toyc ./tests/inline.toy -emit=mlir -mlir-print-debuginfo &> ./tests/ch4_wo_opt.mlir
+# ./build/toyc ./tests/inline.toy -emit=mlir -opt -mlir-print-debuginfo &> ./tests/ch4_inline_only.mlir
+# ./build/toyc ./tests/inline.toy -emit=mlir -opt -infer-shape -mlir-print-debuginfo &> ./tests/ch4_inline_infer_shape.mlir
 
-./build/toyc ./tests/convert.toy -opt -emit=mlir -mlir-print-debuginfo &> ./tests/ch5_origin.mlir
-./build/toyc ./tests/convert.toy -opt -emit=mlir-affine -mlir-print-debuginfo &> ./tests/ch5_affine.mlir
+# ./build/toyc ./tests/convert.toy -opt -emit=mlir -mlir-print-debuginfo &> ./tests/ch5_origin.mlir
+# ./build/toyc ./tests/convert.toy -opt -emit=mlir-affine -mlir-print-debuginfo &> ./tests/ch5_affine.mlir
+
+./build/toyc ./tests/convert.toy -opt -emit=mlir -mlir-print-debuginfo &> ./tests/ch6_origin.mlir
+./build/toyc ./tests/convert.toy -opt -emit=mlir-llvm -mlir-print-debuginfo &> ./tests/ch6_mlir_llvm.mlir
+./build/toyc ./tests/convert.toy -opt -emit=llvm -mlir-print-debuginfo &> ./tests/ch6_llvm.mlir
+echo 'def main() { print([[1, 2], [3, 4]]); }' | ./build/toyc -emit=jit
--- toy_example_execise/toyc.cpp
+++ toy_example_execise/toyc.cpp
@@ -18,6 +18,8 @@
 
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/Dialect/Affine/Passes.h"
+#include "mlir/ExecutionEngine/ExecutionEngine.h"
+#include "mlir/ExecutionEngine/OptUtils.h"
 #include "mlir/IR/AsmState.h"
 #include "mlir/IR/BuiltinOps.h"
 #include "mlir/IR/MLIRContext.h"
@@ -25,13 +27,17 @@
 #include "mlir/Parser/Parser.h"
 #include "mlir/Pass/Pass.h"
 #include "mlir/Pass/PassManager.h"
+#include "mlir/Target/LLVMIR/Dialect/LLVMIR/LLVMToLLVMIRTranslation.h"
+#include "mlir/Target/LLVMIR/Export.h"
 #include "mlir/Transforms/Passes.h"
 
 #include "llvm/ADT/StringRef.h"
+#include "llvm/IR/Module.h"
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/ErrorOr.h"
 #include "llvm/Support/MemoryBuffer.h"
 #include "llvm/Support/SourceMgr.h"
+#include "llvm/Support/TargetSelect.h"
 #include "llvm/Support/raw_ostream.h"
 
 using namespace toy;
@@ -51,14 +57,28 @@ static cl::opt<enum InputType> inputType(
                           "load the input file as an MLIR file")));
 
 namespace {
-enum Action { None, DumpAST, DumpMLIR, DumpMLIRAffine };
+enum Action {
+  None,
+  DumpAST,
+  DumpMLIR,
+  DumpMLIRAffine,
+  DumpMLIRLLVM,
+  DumpLLVMIR,
+  RunJIT
+};
 } // namespace
 static cl::opt<enum Action>
     emitAction("emit", cl::desc("Select the kind of output desired"),
     cl::values(clEnumValN(DumpAST, "ast", "output the AST dump")),
     cl::values(clEnumValN(DumpMLIR, "mlir", "output the MLIR dump")),
     cl::values(clEnumValN(DumpMLIRAffine, "mlir-affine",
-                          "output the MLIR dump after affine lowering")));
+                          "output the MLIR dump after affine lowering")),
+    cl::values(clEnumValN(DumpMLIRLLVM, "mlir-llvm",
+                          "output the MLIR dump after llvm lowering")),
+    cl::values(clEnumValN(DumpLLVMIR, "llvm", "output the LLVM IR dump")),
+    cl::values(
+        clEnumValN(RunJIT, "jit",
+                   "JIT the code and run it by invoking the main function")));
 
 static cl::opt<bool> enableOpt("opt", cl::desc("Enable optimizations"));
 static cl::opt<bool> enableInfer("infer-shape", cl::desc("Enable shape inference"));
@@ -77,7 +97,7 @@ std::unique_ptr<toy::ModuleAST> parseInputFile(llvm::StringRef filename) {
   return parser.parseModule();
 }
 
-int loadMLIR(llvm::SourceMgr &sourceMgr, mlir::MLIRContext &context,
+int loadMLIR(mlir::MLIRContext &context,
              mlir::OwningOpRef<mlir::ModuleOp> &module) {
   // Handle '.toy' input to the compiler.
   if (inputType != InputType::MLIR &&
@@ -100,6 +120,7 @@ int loadMLIR(llvm::SourceMgr &sourceMgr, mlir::MLIRContext &context,
   }
 
   // Parse the input mlir.
+  llvm::SourceMgr sourceMgr;
   sourceMgr.AddNewSourceBuffer(std::move(*fileOrErr), llvm::SMLoc());
   module = mlir::parseSourceFile<mlir::ModuleOp>(sourceMgr, &context);
   if (!module) {
@@ -109,24 +130,18 @@ int loadMLIR(llvm::SourceMgr &sourceMgr, mlir::MLIRContext &context,
   return 0;
 }
 
-int dumpMLIR() {
-  mlir::MLIRContext context;
-  // Load our Dialect in this MLIR Context.
-  context.getOrLoadDialect<mlir::toy::ToyDialect>();
-
-  mlir::OwningOpRef<mlir::ModuleOp> module;
-  llvm::SourceMgr sourceMgr;
-  mlir::SourceMgrDiagnosticHandler sourceMgrHandler(sourceMgr, &context);
-  if (int error = loadMLIR(sourceMgr, context, module))
+int loadAndProcessMLIR(mlir::MLIRContext &context,
+                       mlir::OwningOpRef<mlir::ModuleOp> &module) {
+  if (int error = loadMLIR(context, module))
     return error;
 
   mlir::PassManager pm(&context);
-
   // Apply any generic pass manager command line options and run the pipeline.
   applyPassManagerCLOptions(pm);
 
   // Check to see what granularity of MLIR we are compiling to.
   bool isLoweringToAffine = emitAction >= Action::DumpMLIRAffine;
+  bool isLoweringToLLVM = emitAction >= Action::DumpMLIRLLVM;
 
   if (enableOpt || isLoweringToAffine) {
     // Inline all functions into main and then delete them.
@@ -154,6 +169,11 @@ int dumpMLIR() {
     }
   }
 
+  if (isLoweringToLLVM) {
+    // Finish lowering the toy IR to the LLVM dialect.
+    pm.addPass(mlir::toy::createLowerToLLVMPass());
+  }
+
   if (mlir::failed(pm.run(*module)))
     return 4;
 
@@ -175,6 +195,66 @@ int dumpAST() {
   return 0;
 }
 
+int dumpLLVMIR(mlir::ModuleOp module) {
+  // Register the translation to LLVM IR with the MLIR context.
+  mlir::registerLLVMDialectTranslation(*module->getContext());
+
+  // Convert the module to LLVM IR in a new LLVM IR context.
+  llvm::LLVMContext llvmContext;
+  auto llvmModule = mlir::translateModuleToLLVMIR(module, llvmContext);
+  if (!llvmModule) {
+    llvm::errs() << "Failed to emit LLVM IR\n";
+    return -1;
+  }
+
+  // Initialize LLVM targets.
+  llvm::InitializeNativeTarget();
+  llvm::InitializeNativeTargetAsmPrinter();
+  mlir::ExecutionEngine::setupTargetTriple(llvmModule.get());
+
+  /// Optionally run an optimization pipeline over the llvm module.
+  auto optPipeline = mlir::makeOptimizingTransformer(
+      /*optLevel=*/enableOpt ? 3 : 0, /*sizeLevel=*/0,
+      /*targetMachine=*/nullptr);
+  if (auto err = optPipeline(llvmModule.get())) {
+    llvm::errs() << "Failed to optimize LLVM IR " << err << "\n";
+    return -1;
+  }
+  llvm::errs() << *llvmModule << "\n";
+  return 0;
+}
+
+int runJit(mlir::ModuleOp module) {
+  // Initialize LLVM targets.
+  llvm::InitializeNativeTarget();
+  llvm::InitializeNativeTargetAsmPrinter();
+
+  // Register the translation from MLIR to LLVM IR, which must happen before we
+  // can JIT-compile.
+  mlir::registerLLVMDialectTranslation(*module->getContext());
+
+  // An optimization pipeline to use within the execution engine.
+  auto optPipeline = mlir::makeOptimizingTransformer(
+      /*optLevel=*/enableOpt ? 3 : 0, /*sizeLevel=*/0,
+      /*targetMachine=*/nullptr);
+
+  // Create an MLIR execution engine. The execution engine eagerly JIT-compiles
+  // the module.
+  mlir::ExecutionEngineOptions engineOptions;
+  engineOptions.transformer = optPipeline;
+  auto maybeEngine = mlir::ExecutionEngine::create(module, engineOptions);
+  assert(maybeEngine && "failed to construct an execution engine");
+  auto &engine = maybeEngine.get();
+
+  // Invoke the JIT-compiled function.
+  auto invocationResult = engine->invokePacked("main");
+  if (invocationResult) {
+    llvm::errs() << "JIT invocation failed\n";
+    return -1;
+  }
+
+  return 0;
+}
 
 int main(int argc, char **argv) {
   mlir::registerAsmPrinterCLOptions();
@@ -183,15 +263,32 @@ int main(int argc, char **argv) {
 
   cl::ParseCommandLineOptions(argc, argv, "toy compiler\n");
 
-  switch (emitAction) {
-  case Action::DumpAST:
+  if (emitAction == Action::DumpAST)
     return dumpAST();
-  case Action::DumpMLIR:
-  case Action::DumpMLIRAffine:
-    return dumpMLIR();
-  default:
-    llvm::errs() << "No action specified (parsing only?), use -emit=<action>\n";
+
+  mlir::MLIRContext context;
+  // Load our Dialect in this MLIR Context.
+  context.getOrLoadDialect<mlir::toy::ToyDialect>();
+
+  mlir::OwningOpRef<mlir::ModuleOp> module;
+  if (int error = loadAndProcessMLIR(context, module))
+    return error;
+
+  // If we aren't exporting to non-mlir, then we are done.
+  bool isOutputingMLIR = emitAction <= Action::DumpMLIRLLVM;
+  if (isOutputingMLIR) {
+    module->dump();
+    return 0;
   }
 
-  return 0;
+  // Check to see if we are compiling to LLVM IR.
+  if (emitAction == Action::DumpLLVMIR)
+    return dumpLLVMIR(*module);
+
+  // Otherwise, we must be running the jit.
+  if (emitAction == Action::RunJIT)
+    return runJit(*module);
+
+  llvm::errs() << "No action specified (parsing only?), use -emit=<action>\n";
+  return -1;
 }
